e0156aa3d6f059dbc40f268dbaf7047da2ce403b
misterblizzard <ax-filipe@hotmail.com>
2018-05-20 05:23:29 -0500

    pushing coverage

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log(a)
+const sum = (a, b) => a + b
+const sub = (a, b) => a - b
+const mult = (a, b) => a * b
+const div = (a, b) => (b === 0) ? "division by 0 is not possible" : a / b
+
+export { sum, sub, mult, div }
